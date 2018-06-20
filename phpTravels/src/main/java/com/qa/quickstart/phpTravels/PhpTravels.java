54561e804ee4b555cb169697b6467d47bbdfdd1b
George <ga.timbrell@googlemail.com>
2018-06-20 17:11:57 +0100

    final commit of 20/06 work

--- phpTravels/src/main/java/com/qa/quickstart/phpTravels/PhpTravels.java
+++ phpTravels/src/main/java/com/qa/quickstart/phpTravels/PhpTravels.java
@@ -1,5 +1,46 @@
 package com.qa.quickstart.phpTravels;
 
-public class PhpTravels {
+import java.awt.AWTException;
+import java.awt.Robot;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+
+public class PhpTravels {
+	WebDriver driver;
+	
+	@FindBy(className="select2-input")
+	WebElement searchBar;
+	
+	@FindBy(className="select2-drop")
+	WebElement dropBar;
+	
+	@FindBy(css="html body div#select2-drop.select2-drop.select2-display-none.select2-with-searchbox.select2-drop-active ul.select2-results li.select2-results-dept-0.select2-result.select2-result-unselectable.select2-result-with-children ul.select2-result-sub li.select2-results-dept-1.select2-result.select2-result-selectable")
+	WebElement firstCity;
+	
+	public PhpTravels(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	public void book(String city, int adults, int nights) throws AWTException {
+
+		Robot robot = new Robot();
+
+		Actions action = new Actions(driver);
+		System.out.println(searchBar.getLocation());
+		//action.moveToElement(searchBar);
+		action.moveByOffset(595, 280).click().perform();
+		
+		action.sendKeys(city).perform();
+		
+		firstCity.click();
+		action.moveToElement(searchBar);
+		action.moveByOffset(300, 0).click().perform();
+		
+
+		try { Thread.sleep(2000); } catch(Exception e) {}
+		System.out.println("testing..");
+	}
 }
--- phpTravels/src/test/java/com/qa/quickstart/phpTravels/JavaTests.java
+++ phpTravels/src/test/java/com/qa/quickstart/phpTravels/JavaTests.java
@@ -3,7 +3,10 @@ package com.qa.quickstart.phpTravels;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
 
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
@@ -20,12 +23,10 @@ public class JavaTests {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\Desktop\\Testing\\chromedriver.exe");
 		
 		
-		String filePath = "C:\\Users\\Admin\\Desktop\\demoQAreport.html";
+		String filePath = "C:\\Users\\Admin\\Desktop\\phpTrvalesReport.html";
 		
 		extent = new ExtentReports(filePath, true);
 
-		
-		
 		test = extent.startTest("Verify application Title");
 
 		// add a note to the test
@@ -48,5 +49,19 @@ public class JavaTests {
 		try { Thread.sleep(500); } catch(Exception e) {} 
 		driver.close();
 		extent.flush();
+	}
+	
+	@Test
+	public void book() {
+
+		PhpTravels phpTravels = PageFactory.initElements(driver, PhpTravels.class);
+		
+		driver.manage().window().maximize();
+		String url = "https://www.phptravels.net/";
+		driver.navigate().to(url);
+		
+		try {
+			phpTravels.book("london", 3, 2);
+		}catch(Exception e) {}
 	}
 }
